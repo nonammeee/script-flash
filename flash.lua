ff5e521a5162aac310d5d569035a12abafcec82e
nonammeee <97672364+nonammeee@users.noreply.github.com>
2025-06-26 15:16:59 +0300

    Update flash.lua

--- flash.lua
+++ flash.lua
@@ -14,7 +14,7 @@ local colors = {
 }
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("TSUNAMI EGY", "Blood Theme")
+local Window = Library.CreateLib("Flash EGY", "Blood Theme")
 
 local Tab = Window:NewTab("قائمه 1")
 local Section = Tab:NewSection("سكربتات بروك هيفين")
